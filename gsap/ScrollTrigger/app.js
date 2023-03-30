78131f7ee5d04f97048ca4c0e3bf4d2e1c9a7d9f
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-03-30 08:26:24 +0100

    toggle actions

--- gsap/ScrollTrigger/app.js
+++ gsap/ScrollTrigger/app.js
@@ -1,22 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".square", {
-	// x: 700,
+	x: 700,
 	duration: 3,
 
 	scrollTrigger: {
 		trigger: ".square",
 		// the animation starts when the  top of the trigger element meets the center of thee viewport
-		start: "top 30%",
-		end: () => `+=${document.querySelector(".square").offsetHeight}`,
-		markers: true,
+		start: "top 60%",
+		end: "top 40%",
+		toggleActions: "restart reverse resume reset", // onEnter onLeave onEnterBack onLeaveBack
+		// several toggle action keys are play, resume, reverse, restart, reset, complete, none
+		// end: () => `+=${document.querySelector(".square").offsetHeight}`,
+		// markers: true,
 
-		// markers: {
-		// 	startColor: "purple",
-		// 	endColor: "fuchisa",
-		// 	fontSize: "4rem",
-		// 	indent: 100,
-		// },
-		toggleClass: "red",
+		markers: {
+			startColor: "purple",
+			endColor: "fuchisa",
+			fontSize: "2rem",
+			// indent: 100,
+		},
+		// toggleClass: "red",
 	},
 });
