a89ab55643e6401b544607027bb06abece2f1876
Isaac Ndubuisi <theisaac3n@gmail.com>
2023-03-30 07:56:46 +0100

    start and end properties along with custom markers

--- gsap/ScrollTrigger/app.js
+++ gsap/ScrollTrigger/app.js
@@ -1,8 +1,22 @@
-gsap.registerPlugin(scrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("square", {
-	x: 700,
+gsap.to(".square", {
+	// x: 700,
 	duration: 3,
 
-	scrollTrigger: ".square2",
+	scrollTrigger: {
+		trigger: ".square",
+		// the animation starts when the  top of the trigger element meets the center of thee viewport
+		start: "top 30%",
+		end: "center 20%",
+		markers: true,
+
+		// markers: {
+		// 	startColor: "purple",
+		// 	endColor: "fuchisa",
+		// 	fontSize: "4rem",
+		// 	indent: 100,
+		// },
+		toggleClass: "red",
+	},
 });
